59ed084b16783d5704f7044afe553d149764a7cd
Amit Thakkar <vigildbest@gmail.com>
2016-04-21 18:40:58 +0530

    1. Removing .js and .js.map files. 2. Requiring Service into component.

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,7 @@
 /**
  * Created by namita on 4/21/16.
  */
-
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {ToDoService} from "./todo-service";
 
-bootstrap(AppComponent,[ToDoService]);
+bootstrap(AppComponent);
--- app/todo-service.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by namita on 4/21/16.
- */
-imp;
-//# sourceMappingURL=todo-service.js.map
